8f354ffcd0edb30356c70d5e38736b89a71d4519
Christopher Krause <christopher2.krause@tu-dortmund.de>
2019-04-15 15:42:11 +0200

    Auswertung

--- V53-Mikrowellen/plot.py
+++ V53-Mikrowellen/plot.py
@@ -1,21 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+U, A, U2, A2, U3, A3 = np.genfromtxt('spannungen.txt', unpack=True)
+#
+def f(x, a, b, c):
+    return a*x**2 + b*x + c
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+x_plot = np.linspace(70, 250, 500)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+params, covariance_matrix = curve_fit(f, U, A)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+#print(params)
+#print(np.sqrt(np.diag(covariance_matrix)))
+#print('U_0 gleich ', params[0], ' +- ', errors[0])
+#plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(U , A, 'r.', label='Messwerte', Markersize=4)
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+
+params, covariance_matrix = curve_fit(f, U2, A2)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', linewidth=0.5)
+plt.plot(U2 , A2, 'r.', Markersize=4)
+
+params, covariance_matrix = curve_fit(f, U3, A3)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', linewidth=0.5)
+plt.plot(U3 , A3, 'r.', Markersize=4)
+
+plt.title('Parabeln')
+plt.legend()
+plt.grid()
+plt.ylim(-5, 160)
+#plt.xlim((0, 400))
+plt.xlabel(r'$U/$V')
+plt.ylabel(r'$U/$mV')
+plt.savefig('build/plot1.pdf')
