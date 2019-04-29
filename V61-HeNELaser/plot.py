a33e632de6f26a05b153782db9a5a315d7bc6492
Christopher Krause <christopher2.krause@tu-dortmund.de>
2019-04-29 18:42:47 +0200

    Auswertung

--- V61-HeNELaser/plot.py
+++ V61-HeNELaser/plot.py
@@ -1,21 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import mu_0
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+L_1, I_1 = np.genfromtxt('konkav.txt', unpack=True)
+L_2, I_2 = np.genfromtxt('planar.txt', unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+def f_1(L, a, b, c):
+   return a * L**2 + b*L + c
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+x_plot = np.linspace(55, 140)
+params, covariance_matrix = curve_fit(f_1, L_1, I_1)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f_1(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print('a')
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(L_1, I_1, 'r.', label='Messwerte', Markersize=4)
+plt.legend()
+plt.grid()
+plt.xlim((56, 138))
+plt.ylabel(r'$I / \mathrm{\mu A}$')
+plt.xlabel(r'$L / $cm')
+plt.savefig('build/stabilitaet.pdf')
+plt.clf()
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+def f_2(L, d, e):
+   return d * L + e
+
+x_plot = np.linspace(55, 100)
+params, covariance_matrix = curve_fit(f_2, L_2, I_2)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f_2(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print('b')
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(L_2, I_2, 'r.', label='Messwerte', Markersize=4)
+plt.legend()
+plt.grid()
+plt.xlim((56, 95))
+plt.ylabel(r'$I / \mathrm{\mu A}$')
+plt.xlabel(r'$L / $cm')
+plt.savefig('build/stabilitaet_2.pdf')
