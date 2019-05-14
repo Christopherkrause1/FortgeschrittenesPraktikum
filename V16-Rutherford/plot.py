9c93bf954cdcd8388c4ec1f1ae349ed5998cbd70
Christopher Krause <christopher2.krause@tu-dortmund.de>
2019-05-14 10:58:33 +0200

    auswertung

--- V16-Rutherford/plot.py
+++ V16-Rutherford/plot.py
@@ -1,21 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import mu_0
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from uncertainties.umath import *
+import uncertainties.unumpy as unp
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+N, W, t = np.genfromtxt('winkel.txt', unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+def f_1(W):
+   return 1/(4*np.pi*np.epsilon_0)**2 *(2*79*(1.6*10**(-19))**2 /(4*1))**2 * 1/(np.sine(W/2))**4
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+x_plot = np.linspace(0, 22)
+#params, covariance_matrix = curve_fit(f_1, W)
+#errors = np.sqrt(np.diag(covariance_matrix))
+#plt.plot(x_plot, f_1(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+#print(params)
+#print(np.sqrt(np.diag(covariance_matrix)))
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.plot(W, N/t, 'r.', label='Messwerte', Markersize=4)
+plt.legend()
+plt.grid()
+plt.ylabel(r'$\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}/ \mathrm{cm^2}$')
+plt.xlabel(r'$\Theta / $°')
+plt.savefig('build/winkel.pdf')
+plt.clf()
