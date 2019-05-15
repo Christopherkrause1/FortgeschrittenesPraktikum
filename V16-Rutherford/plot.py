afb30ae50f580ed664b134949edc0b287c7d3c2a
Lucas Witthaus <lucas.witthaus@udo.edu>
2019-05-15 16:45:58 +0200

    asödj

--- V16-Rutherford/plot.py
+++ V16-Rutherford/plot.py
@@ -10,7 +10,6 @@ N, W, t = np.genfromtxt('winkel.txt', unpack=True)
 N0 = 6249/300
 #print(N0, '+-', np.sqrt(6249)/300)
 #print(N/t)
-err = np.sqrt(N)/t
 
 #effektive Detektorfläche:
 #Breite
@@ -33,12 +32,13 @@ omega = 4 * np.arctan((b*h)/(2*d*np.sqrt(4*d**2 + b**2 + h**2)))
 dicke = 2 * 10**(-6)
 
 #Atomdichte
-dichte = 5.895*10**(28)
+dichte = 5.907*10**(28)
 
 #diff Wirkungsquerschnitt:
 dWq = N/(t*N0*dichte*dicke*omega)
+y_err = np.sqrt((np.sqrt(N)/(t*N0*dichte*dicke*omega))**2 + (0.26/(t*N0*dichte*dicke*omega))**2)
 print(dWq)
-
+print(y_err)
 def f_1(y):
    return 1/(4*np.pi*8.854*10**(-12))**2 *((2*79*1.602*10**(-19)) /(4*5.638*10**6))**2 * 1/(np.sin(y/2))**4
 
@@ -48,9 +48,9 @@ x_plot = np.linspace(0.01, 22,200)
 #plt.plot(x_plot, f_1(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
 #print(params)
 #print(np.sqrt(np.diag(covariance_matrix)))
-plt.gcf().subplots_adjust(bottom=0.18)
+plt.gcf().subplots_adjust(bottom=0.18, left  = 0.14)
 plt.plot(x_plot, f_1(x_plot/360*2*np.pi), 'k-', label='Theoriekurve')
-plt.plot(W, dWq, 'r.', label='Messwerte', Markersize=4)
+plt.errorbar(W, dWq, yerr=y_err, fmt = 'o',color='r', markersize=2, capsize=2, ecolor='b', elinewidth=0.5, markeredgewidth=0.5, label='berechnete Werte')
 plt.legend()
 plt.grid()
 plt.yscale('log')
@@ -60,3 +60,13 @@ plt.ylabel(r'$\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}/ \mathrm{cm^2}$')
 plt.xlabel(r'$\Theta / $°')
 plt.savefig('build/winkel.pdf')
 plt.clf()
+
+
+#Mehrfachstreuung
+dicke = 4 * 10**(-6)
+N = 1033
+t = 300
+dWq = N/(t*N0*dichte*dicke*omega)
+err = np.sqrt((np.sqrt(N)/(t*N0*dichte*dicke*omega))**2 + (0.26/(t*N0*dichte*dicke*omega))**2)
+print(dWq)
+print(err)
