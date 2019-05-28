f6d0402256e7ac2cd54bb371ffb4a561da6ea627
Lucas Witthaus <lucas.witthaus@udo.edu>
2019-05-28 15:14:26 +0200

    erster teil auswertung

--- V14-Tomographie/plot.py
+++ V14-Tomographie/plot.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import mu_0
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from uncertainties.umath import *
+import uncertainties.unumpy as unp
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+N = np.genfromtxt('verlauf.txt', unpack=True)
+x = np.linspace(0.5,146.5,147)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-
+plt.plot(x,N,'k-', label='Messwerte', linewidth=1)
+plt.grid()
+plt.legend()
+plt.xlabel('Channel')
+plt.ylabel('Counts')
+plt.xlim(0,147)
 # in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.savefig('build/verlauf.pdf')
