447cc4dec1e4957de38cf16b3e6c18bfd2ae9d4e
Lucas Witthaus <lucas.witthaus@udo.edu>
2019-06-13 12:51:57 +0200

    plots

--- V46-Faraday/plot.py
+++ V46-Faraday/plot.py
@@ -7,34 +7,34 @@ from uncertainties.umath import *
 import uncertainties.unumpy as unp
 
 #Einlesen Daten reine Probe
-gradl, minutenl, gradr, minutenr, lamb = np.genfromtxt('rein.txt', unpack=True)
+gradl_rein, minutenl_rein, gradr_rein, minutenr_rein, lamb = np.genfromtxt('rein.txt', unpack=True)
 L = 0.0051
 print('Wellenlaengen:')
 print(lamb)
 print('--------------------------------')
 #Berechnung der Winkel reine Probe
-thetal = gradl + minutenl/60
-thetar = gradr + minutenr/60
+thetal_rein = gradl_rein + minutenl_rein/60
+thetar_rein = gradr_rein + minutenr_rein/60
 #Winkel in rad umrechnen
-thetal = thetal/360 * 2*np.pi
-thetar = thetar/360 * 2*np.pi
+thetal_rein = thetal_rein/360 * 2*np.pi
+thetar_rein = thetar_rein/360 * 2*np.pi
 #Berechnung des Drehwinkels
-theta = 1/2*(thetar - thetal)
+theta_rein = 1/2*(thetar_rein - thetal_rein)
 #Drehwinkel pro Länge
-theta_frei = theta/L
+theta_frei_rein = theta_rein/L
 #Ausgabe der Winkel
 print('Reine Probe:')
 print('Theta links in rad:')
-print(thetal)
+print(thetal_rein)
 print('Theta rechts in rad:')
-print(thetar)
+print(thetar_rein)
 print('Drehwinkel in rad:')
-print(theta)
+print(theta_rein)
 print('Drehwinkel pro Laenge:')
-print(theta_frei)
+print(theta_frei_rein)
 print('------------------------------------------------')
 
-plt.plot(lamb**2, theta_frei, 'r.', label='Messwerte', Markersize=4)
+plt.plot(lamb**2, theta_frei_rein, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
 plt.gcf().subplots_adjust(bottom=0.18)
@@ -43,31 +43,31 @@ plt.ylabel(r'$\frac{\Theta}{L} / \frac{\mathrm{rad}}{\mathrm{m}}$')
 plt.savefig('build/rein.pdf')
 plt.clf()
 
-gradl, minutenl, gradr, minutenr = np.genfromtxt('dotiert136.txt', unpack=True)
+gradl_dotiert136, minutenl_dotiert136, gradr_dotiert136, minutenr_dotiert136 = np.genfromtxt('dotiert136.txt', unpack=True)
 L = 0.00136
 #Berechnung der Winkel reine Probe
-thetal = gradl + minutenl/60
-thetar = gradr + minutenr/60
+thetal_dotiert136 = gradl_dotiert136 + minutenl_dotiert136/60
+thetar_dotiert136 = gradr_dotiert136 + minutenr_dotiert136/60
 #Winkel in rad umrechnen
-thetal = thetal/360 * 2*np.pi
-thetar = thetar/360 * 2*np.pi
+thetal_dotiert136 = thetal_dotiert136/360 * 2*np.pi
+thetar_dotiert136 = thetar_dotiert136/360 * 2*np.pi
 #Berechnung des Drehwinkels
-theta = 1/2*(thetar - thetal)
+theta_dotiert136 = 1/2*(thetar_dotiert136 - thetal_dotiert136)
 #Drehwinkel pro Länge
-theta_frei = theta/L
+theta_frei_dotiert136 = theta_dotiert136/L
 #Ausgabe der Winkel
 print('Dotierte Probe der Dicke 1,36mm:')
 print('Theta links in rad:')
-print(thetal)
+print(thetal_dotiert136)
 print('Theta rechts in rad:')
-print(thetar)
+print(thetar_dotiert136)
 print('Drehwinkel in rad:')
-print(theta)
+print(theta_dotiert136)
 print('Drehwinkel pro Laenge:')
-print(theta_frei)
+print(theta_frei_dotiert136)
 print('------------------------------------------------')
 
-plt.plot(lamb**2, theta_frei, 'r.', label='Messwerte', Markersize=4)
+plt.plot(lamb**2, theta_frei_dotiert136, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
 plt.gcf().subplots_adjust(bottom=0.18)
@@ -76,30 +76,30 @@ plt.ylabel(r'$\frac{\Theta}{L} / \frac{\mathrm{rad}}{\mathrm{m}}$')
 plt.savefig('build/dotiert136.pdf')
 plt.clf()
 
-gradl, minutenl, gradr, minutenr = np.genfromtxt('dotiert1296.txt', unpack=True)
+gradl_dotiert1296, minutenl_dotiert1296, gradr_dotiert1296, minutenr_dotiert1296 = np.genfromtxt('dotiert1296.txt', unpack=True)
 L = 0.001296
 #Berechnung der Winkel reine Probe
-thetal = gradl + minutenl/60
-thetar = gradr + minutenr/60
+thetal_dotiert1296 = gradl_dotiert1296 + minutenl_dotiert1296/60
+thetar_dotiert1296 = gradr_dotiert1296 + minutenr_dotiert1296/60
 #Winkel in rad umrechnen
-thetal = thetal/360 * 2*np.pi
-thetar = thetar/360 * 2*np.pi
+thetal_dotiert1296 = thetal_dotiert1296/360 * 2*np.pi
+thetar_dotiert1296 = thetar_dotiert1296/360 * 2*np.pi
 #Berechnung des Drehwinkels
-theta = 1/2*(thetar - thetal)
+theta_dotiert1296 = 1/2*(thetar_dotiert1296 - thetal_dotiert1296)
 #Drehwinkel pro Länge
-theta_frei = theta/L
+theta_frei_dotiert1296 = theta_dotiert1296/L
 #Ausgabe der Winkel
 print('Dotierte Probe der Dicke 1,296mm:')
 print('Theta links in rad:')
-print(thetal)
+print(thetal_dotiert1296)
 print('Theta rechts in rad:')
-print(thetar)
+print(thetar_dotiert1296)
 print('Drehwinkel in rad:')
-print(theta)
+print(theta_dotiert1296)
 print('Drehwinkel pro Laenge:')
-print(theta_frei)
+print(theta_frei_dotiert1296)
 
-plt.plot(lamb**2, theta_frei, 'r.', label='Messwerte', Markersize=4)
+plt.plot(lamb**2, theta_frei_dotiert1296, 'r.', label='Messwerte', Markersize=4)
 plt.legend()
 plt.grid()
 plt.gcf().subplots_adjust(bottom=0.18)
@@ -107,3 +107,53 @@ plt.xlabel(r'$\lambda^2 / \mu m^2$')
 plt.ylabel(r'$\frac{\Theta}{L} / \frac{\mathrm{rad}}{\mathrm{m}}$')
 plt.savefig('build/dotiert1296.pdf')
 plt.clf()
+
+#Abziehen der Dotierung Probe der Dicke 1,36mm
+theta = theta_frei_dotiert136 - theta_frei_rein
+def f(x, a):
+    return a*x
+
+x_plot = np.linspace(1, 8)
+params, covariance_matrix = curve_fit(f, lamb**2, theta)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print('-------------------------------------------')
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+print('-------------------------------------------')
+
+plt.plot(lamb**2, theta, 'r.', label='Messwerte', Markersize=4)
+plt.legend()
+plt.grid()
+plt.xlim(1,8)
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.xlabel(r'$\lambda^2 / \mu m^2$')
+plt.ylabel(r'$\frac{\Theta}{L} / \frac{\mathrm{rad}}{\mathrm{m}}$')
+plt.savefig('build/differenz136.pdf')
+plt.clf()
+
+a_136 = ufloat(6.38309048, 1.09034248) * 10**(12)
+
+#Abziehen der Dotierung Probe der Dicke 1,36mm
+theta = theta_frei_dotiert1296 - theta_frei_rein
+
+x_plot = np.linspace(1, 8)
+params, covariance_matrix = curve_fit(f, lamb**2, theta)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Anpassungsfunktion', linewidth=0.5)
+print('-------------------------------------------')
+print(params)
+print(np.sqrt(np.diag(covariance_matrix)))
+print('-------------------------------------------')
+
+plt.plot(lamb**2, theta, 'r.', label='Messwerte', Markersize=4)
+plt.legend()
+plt.grid()
+plt.xlim(1,8)
+plt.gcf().subplots_adjust(bottom=0.18)
+plt.xlabel(r'$\lambda^2 / \mu m^2$')
+plt.ylabel(r'$\frac{\Theta}{L} / \frac{\mathrm{rad}}{\mathrm{m}}$')
+plt.savefig('build/differenz1296.pdf')
+plt.clf()
+
+a_1296 = ufloat(11.74260897, 1.6360627) * 10**(12)
